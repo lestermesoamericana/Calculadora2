123fda8691bb7a96219c2d6770254de9d87b8b7f
lestermesoamericana <66986117+lestermesoamericana@users.noreply.github.com>
2020-07-02 15:30:48 -0600

    Uso de parser
    We just has used the parser library just to evaluete the operations that are receiving from display. The parser allow us to compare the expression that is within display so it can operate just like any other math expression. You only have to be sure you're introducing valid math values

--- disenyo.py
+++ disenyo.py
@@ -1,5 +1,7 @@
 # Creacion de una calculadora en Python
 from tkinter import *
+# este modulo me permite tomar una expreison y empezar a evaluarla
+import parser
 
 # creacion de raiz para el frame principal
 raiz = Tk()
@@ -20,86 +22,133 @@ pantalla.grid(row = 1, columnspan = 6, sticky = W + E)
 i = 0
 
 # -------------------------- funcion poner numeros --------------------------
-def getnumber(num):
+def get_number(num):
     global i
 
     pantalla.insert(i, num)
     i+=1
 
+# ---------------------------- funcion operador ----------------------------
+def get_operador(operator):
+    global i
+
+    operator_lenght = len(operator)
+    pantalla.insert(i, operator)
+    i+= operator_lenght
+
+# ------------------------- funcion LimpiarPantalla -------------------------
+# para toda la pantalla
+def limpiar_pantalla():
+    pantalla.delete(0, END)
+
+# para un solo caracter
+def undo_character():
+    estado_pantalla = pantalla.get()
+    # si la pantalla tiene datos
+    if len(estado_pantalla):
+        # quitar un elemento de la pantalla
+        nueva_pantalla = estado_pantalla[:-1]
+        # limpiamos la pantalla
+        limpiar_pantalla()
+        # y lo ingresa a la pantalla
+        pantalla.insert(0,nueva_pantalla)
+    else:
+        limpiar_pantalla()
+        pantalla.insert(0, "ERROR")
+
+# ---------------------------- funcion Calcular ----------------------------
+def calcular():
+    estado_pantalla = pantalla.get()
+    try:
+        expresion_matematica = parser.expr(estado_pantalla).compile()
+        result = eval(expresion_matematica)
+        limpiar_pantalla()
+        pantalla.insert(0,result)
+    except:
+        limpiar_pantalla()
+        pantalla.insert(0,"ERROR")
+
 #------------------------ Fila1 ---------------------------------------------
 # borrar memoria
-Button(miFrame, text = "C").grid(row = 2, column = 0, sticky = W+E)
+Button(miFrame, text = "^2", command = lambda: get_operador("**2")).grid(row = 2, 
+    column = 0, sticky = W+E)
 
 # borrar dato ingresado
-Button(miFrame, text = "CE").grid(row = 2, column = 1, sticky = W+E)
+Button(miFrame, text = "CE", command = lambda: limpiar_pantalla()).grid(row = 2, column = 1, sticky = W+E)
 
 # borrar caracter
-Button(miFrame, text = "←").grid(row = 2, column = 2, sticky = W+E)
+Button(miFrame, text = "←", command = lambda : undo_character()).grid(row = 2, column = 2, sticky = W+E)
 
 # division
-Button(miFrame, text = "➗").grid(row = 2, column = 3, sticky = W+E, columnspan = 3)
+Button(miFrame, text = "➗", command = lambda : get_operador("/")).grid(row = 2, 
+    column = 3, sticky = W+E, columnspan = 3)
 
 #------------------------ Fila2 ---------------------------------------------
 # numero 7
-Button(miFrame, text = "7", command = lambda :getnumber(7)).grid(row = 3, 
+Button(miFrame, text = "7", command = lambda :get_number(7)).grid(row = 3, 
     column = 0, sticky = W+E) 
 
 # numero 8
-Button(miFrame, text = "8", command = lambda :getnumber(8)).grid(row = 3, 
+Button(miFrame, text = "8", command = lambda :get_number(8)).grid(row = 3, 
     column = 1, sticky = W+E)
 
 # Numero 9
-Button(miFrame, text = "9", command = lambda :getnumber(9)).grid(row = 3,
+Button(miFrame, text = "9", command = lambda :get_number(9)).grid(row = 3,
     column = 2, sticky = W+E)
 
 # Multiplicacion
-Button(miFrame, text = "*").grid(row = 3, column = 3, sticky = W+E, columnspan = 3)
+Button(miFrame, text = "*", command = lambda : get_operador("*")).grid(row = 3, 
+    column = 3, sticky = W+E, columnspan = 3)
 
 #------------------------ Fila3 ---------------------------------------------
 # Numero 4
-Button(miFrame, text = "4", command = lambda :getnumber(4)).grid(row = 4, 
+Button(miFrame, text = "4", command = lambda :get_number(4)).grid(row = 4, 
     column = 0, sticky = W+E)
 
 # Numero 5
-Button(miFrame, text = "5", command = lambda :getnumber(5)).grid(row = 4, 
+Button(miFrame, text = "5", command = lambda :get_number(5)).grid(row = 4, 
     column = 1, sticky = W+E)
 
 # Numero 6
-Button(miFrame, text = "6", command = lambda :getnumber(6)).grid(row = 4, 
+Button(miFrame, text = "6", command = lambda :get_number(6)).grid(row = 4, 
     column = 2, sticky = W+E)
 
 # Resta
-Button(miFrame, text = "-").grid(row = 4, column = 3, sticky = W+E, columnspan = 3)
+Button(miFrame, text = "-", command = lambda : get_operador("-")).grid(row = 4, 
+    column = 3, sticky = W+E, columnspan = 3)
 
 #------------------------ Fila4 ---------------------------------------------
 # numero 1
-Button(miFrame, text = "1", command = lambda :getnumber(1)).grid(row = 5, 
+Button(miFrame, text = "1", command = lambda :get_number(1)).grid(row = 5, 
     column = 0, sticky = W+E)
 
 # numero 2
-Button(miFrame, text = "2", command = lambda :getnumber(2)).grid(row = 5, 
+Button(miFrame, text = "2", command = lambda :get_number(2)).grid(row = 5, 
     column = 1, sticky = W+E)
 
 # numero 3
-Button(miFrame, text = "3", command = lambda :getnumber(3)).grid(row = 5, 
+Button(miFrame, text = "3", command = lambda :get_number(3)).grid(row = 5, 
     column = 2, sticky = W+E)
 
 # suma
-Button(miFrame, text = "+").grid(row = 5, column = 3, sticky = W+E, columnspan = 3)
+Button(miFrame, text = "+", command = lambda : get_operador("+")).grid(row = 5, 
+    column = 3, sticky = W+E, columnspan = 3)
 
 #------------------------ Fila5 ---------------------------------------------
 # boton 0
-Button(miFrame, text = "0", command = lambda :getnumber(0)).grid(row = 6, 
+Button(miFrame, text = "0", command = lambda :get_number(0)).grid(row = 6, 
     column = 0, sticky = W+E)
 
 # boton Punto (.)
 Button(miFrame, text = ".").grid(row = 6, column = 1, sticky = W+E)
 
 # boton %
-Button(miFrame, text = "%").grid(row = 6, column = 2, sticky = W+E)
+Button(miFrame, text = "%", command = lambda : get_operador("+")).grid(row = 6, 
+    column = 2, sticky = W+E)
 
 # boton igual
-Button(miFrame, text = "=").grid(row = 6, column = 3, sticky = W+E, columnspan = 3)
+Button(miFrame, text = "=", command = lambda : calcular()).grid(row = 6, 
+    column = 3, sticky = W+E, columnspan = 3)
 
 # Final del codigo
 raiz.mainloop()
